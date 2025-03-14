c2cd58f0def5e126e640374047b24d5b1381a03e
qStivi <stephanglaue@googlemail.com>
2025-03-14 23:19:36 +0100

    Now working python / java connection & loop

--- src/main/java/de/uni_marburg/schematch/BayesianOptimization.java
+++ src/main/java/de/uni_marburg/schematch/BayesianOptimization.java
@@ -21,15 +21,16 @@ import org.json.JSONObject;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BayesianOptimization {
     private static final Logger log = LogManager.getLogger(BayesianOptimization.class);
     private static final String CLIENT_HOST = "localhost";
-    private static final int SEND_PORT = 5004;
+    private static final int SEND_PORT = 5003;
     private static final int LISTEN_PORT = 5005;
-    private static Process pythonProcess;
     private final Socket sendClient;
+    private final PrintWriter sendWriter;  // Persistent writer for sending data
     private final ServerSocket listenServer;
     private final SimilarityFlooding sf;
     private final Configuration config;
@@ -37,6 +38,13 @@ public class BayesianOptimization {
     public BayesianOptimization() {
         this.listenServer = initListenServer(LISTEN_PORT);
         this.sendClient = initSendClient(CLIENT_HOST, SEND_PORT);
+        // Create a persistent PrintWriter without auto-closing the stream
+        try {
+            this.sendWriter = new PrintWriter(new OutputStreamWriter(sendClient.getOutputStream()), true);
+        } catch (IOException e) {
+            log.error("Failed to get output stream from sendClient: ", e);
+            throw new RuntimeException(e);
+        }
         this.sf = new SimilarityFlooding();
         this.config = Configuration.getInstance();
     }
@@ -61,6 +69,26 @@ public class BayesianOptimization {
         }
     }
 
+    private static ServerSocket initListenServer(int serverPort) {
+        log.info("Initializing server on port {}", serverPort);
+        try {
+            return new ServerSocket(serverPort);
+        } catch (IOException e) {
+            log.error("Failed to start server: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Socket initSendClient(String clientHost, int clientPort) {
+        log.info("Connecting to client at {}:{}", clientHost, clientPort);
+        try {
+            return new Socket(clientHost, clientPort);
+        } catch (IOException e) {
+            log.error("Failed to connect to client: ", e);
+            throw new RuntimeException(e);
+        }
+    }
+
     private void sendInitialData() {
         log.info("Collecting initial data");
         var score = getAvgPerformance();
@@ -74,62 +102,10 @@ public class BayesianOptimization {
         initialData.put("possible_values", possibleParams);
         log.info("JSON DATA: {}", initialData);
 
-        log.info("Starting output stream");
-        try (OutputStream output = sendClient.getOutputStream(); PrintWriter writer = new PrintWriter(new OutputStreamWriter(output), true)) {
-            log.info("Writing initial data");
-            writer.println(initialData);
-            log.info("Flushing writer");
-            writer.flush();
-        } catch (IOException e) {
-            log.error("Failed to send initial data: ", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static ServerSocket initListenServer(int serverPort) {
-        log.info("Initializing server on port {}", serverPort);
-        try {
-            return new ServerSocket(serverPort);
-        } catch (IOException e) {
-            log.error("Failed to start server: ", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void startPythonScript() {
-        log.info("Starting python script");
-        try {
-            ProcessBuilder pb = new ProcessBuilder("scripts/Optimizations/.venv/bin/python", "scripts/Optimizations/BayesianOptimization.py");
-            pb.redirectErrorStream(true);
-            pythonProcess = pb.start();
-
-            // Chill for a bit to let the Python script start up
-            Thread.sleep(2000);
-
-            new Thread(() -> {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(pythonProcess.getInputStream()))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        log.info("Python: " + line);
-                    }
-                } catch (IOException e) {
-                    log.error("Error reading Python output: ", e);
-                }
-            }).start();
-
-            log.info("Python optimization script started.");
-        } catch (IOException e) {
-            log.error("Failed to start Python script: ", e);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void stopPythonScript() {
-        if (pythonProcess != null && pythonProcess.isAlive()) {
-            pythonProcess.destroy();
-            log.info("Python script terminated.");
-        }
+        // Use the persistent sendWriter without closing it.
+        log.info("Sending initial data using persistent connection");
+        sendWriter.println(initialData);
+        sendWriter.flush();
     }
 
     private void configureSimilarityFlooding() {
@@ -144,203 +120,6 @@ public class BayesianOptimization {
         this.sf.setINDV2("false");
     }
 
-//    private static void optimizeParameters(SimilarityFlooding similarityFlooding, ThresholdSelectionBoosting thresholdSelectionBoosting) {
-//        try (Socket socket = new Socket(HOST, PORT); BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); PrintWriter writer = new PrintWriter(socket.getOutputStream(), true)) {
-//
-//            Map<String, String> currentParams = similarityFlooding.getParameters();
-//            Map<String, Collection<String>> possibleParams = similarityFlooding.getPossibleValues();
-//            double bestScore = 0.0;
-//            Map<String, String> bestParams = new HashMap<>();
-//
-//            double avgPerformance = getAvgPerformance(thresholdSelectionBoosting);
-//
-//            var jsonString = convertToJsonString(currentParams);
-//            var jsonString2 = convertToJsonString2(possibleParams);
-//
-//            log.info("Aktuelle Parameter: {}, Score: {}", currentParams, avgPerformance);
-//            writer.println(avgPerformance + "|" + jsonString + "|" + jsonString2);
-//
-////            String inputLine;
-////            StringBuilder response = new StringBuilder();
-////            while ((inputLine = reader.readLine()) != null) {
-////                response.append(inputLine);
-////            }
-////            log.info("Response: {}", response);
-//
-//
-////                log.info("Response: {}", response);
-////                if (response.equals("DONE")) {
-////                    break;
-////                }
-////
-//////                currentParams = parseParameters(response);
-////                similarityFlooding.setParameters(currentParams);
-////
-////                if (avgPerformance > bestScore) {
-////                    bestScore = avgPerformance;
-////                    bestParams = new HashMap<>(currentParams);
-////                }
-//
-//            log.info("Optimierung abgeschlossen. Beste Parameter: {}, Score: {}", bestParams, bestScore);
-//        } catch (IOException e) {
-//            log.error("Exception during optimization: ", e);
-//        }
-//
-//        // new server to recive the parameters
-//        try {
-//            int port = 5003;
-//            try (ServerSocket serverSocket = new ServerSocket(port)) {
-//                System.out.println("Server started and listening on port " + port);
-//                while (true) {
-//                    try (Socket clientSocket = serverSocket.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-//
-//                        System.out.println("Client connected");
-//
-//                        // Read the input from the client
-//                        String inputLine;
-//                        StringBuilder data = new StringBuilder();
-//                        while ((inputLine = in.readLine()) != null) {
-//                            data.append(inputLine);
-//                        }
-//
-//                        // Parse the JSON data
-//                        JSONObject json = new JSONObject(data.toString());
-//                        System.out.println("Received JSON: " + json);
-//
-//                        // Process the JSON data as needed
-//                        // For example, extract parameters
-//                        String fixpoint = json.getString("fixpoint");
-//                        boolean indv2 = json.getBoolean("INDV2");
-//                        // ... extract other parameters
-//
-//                        // Send a response back to the client
-//                        out.println("Response from Java server");
-//
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//
-//        /*
-//        import java.io.BufferedReader;
-//import java.io.InputStreamReader;
-//import java.io.PrintWriter;
-//import java.net.ServerSocket;
-//import java.net.Socket;
-//import org.json.JSONObject;
-//
-//public class JavaServer {
-//    public static void main(String[] args) {
-//        int port = 5003;
-//        try (ServerSocket serverSocket = new ServerSocket(port)) {
-//            System.out.println("Server started and listening on port " + port);
-//            while (true) {
-//                try (Socket clientSocket = serverSocket.accept();
-//                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-//                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
-//
-//                    System.out.println("Client connected");
-//
-//                    // Read the input from the client
-//                    String inputLine;
-//                    StringBuilder data = new StringBuilder();
-//                    while ((inputLine = in.readLine()) != null) {
-//                        data.append(inputLine);
-//                    }
-//
-//                    // Parse the JSON data
-//                    JSONObject json = new JSONObject(data.toString());
-//                    System.out.println("Received JSON: " + json.toString());
-//
-//                    // Process the JSON data as needed
-//                    // For example, extract parameters
-//                    String fixpoint = json.getString("fixpoint");
-//                    boolean indv2 = json.getBoolean("INDV2");
-//                    // ... extract other parameters
-//
-//                    // Send a response back to the client
-//                    out.println("Response from Java server");
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
-//         */
-//    }
-
-//    private static String convertToJsonString2(Map<String, Collection<String>> possibleParams) {
-//        var jsonString = new StringBuilder("{");
-//        for (Iterator<Map.Entry<String, Collection<String>>> iterator = possibleParams.entrySet().iterator(); iterator.hasNext(); ) {
-//            Map.Entry<String, Collection<String>> entry = iterator.next();
-//            jsonString.append("\"").append(entry.getKey()).append("\": [");
-//            for (Iterator<String> iter = entry.getValue().iterator(); iter.hasNext(); ) {
-//                String value = iter.next();
-//                jsonString.append("\"").append(value);
-//                if (iter.hasNext()) {
-//                    jsonString.append("\", ");
-//                } else {
-//                    jsonString.append("\"");
-//                }
-//            }
-//            jsonString.append("]");
-//            if (iterator.hasNext()) {
-//                jsonString.append(", ");
-//            }
-//        }
-//        jsonString.append("}");
-//        return jsonString.toString();
-//    }
-//
-//    private static String convertToJsonString(Map<String, String> currentParams) {
-//        var jsonString = new StringBuilder("{");
-//        for (Iterator<Map.Entry<String, String>> iterator = currentParams.entrySet().iterator(); iterator.hasNext(); ) {
-//            Map.Entry<String, String> entry = iterator.next();
-//            jsonString.append("\"").append(entry.getKey()).append("\": \"").append(entry.getValue());
-//            if (iterator.hasNext()) {
-//                jsonString.append("\", ");
-//            } else {
-//                jsonString.append("\"");
-//            }
-//        }
-//        jsonString.append("}");
-//        return jsonString.toString();
-//    }
-//
-//    private static Map<String, String> parseParameters(String input) {
-//        Map<String, String> params = new HashMap<>();
-//        input = input.replace("{", "").replace("}", "");
-//        for (String pair : input.split(", ")) {
-//            String[] kv = pair.split("=");
-//            if (kv.length == 2) {
-//                params.put(kv[0].trim(), kv[1].trim());
-//            }
-//        }
-//        return params;
-//    }
-
-    private Socket initSendClient(String clientHost, int clientPort) {
-        log.info("Connecting to client at {}:{}", clientHost, clientPort);
-        try {
-            return new Socket(clientHost, clientPort);
-        } catch (IOException e) {
-            log.error("Failed to connect to client: ", e);
-            throw new RuntimeException(e);
-        }
-    }
-
     private Thread startListingThread() {
         return new Thread(() -> {
             try {
@@ -352,83 +131,49 @@ public class BayesianOptimization {
     }
 
     private void startListening() {
-        /*
-        1. Start a server socket on a port
-        2. Accept incoming connections
-        3. Read the input from the client
-        4. Parse the JSON data
-        5. Calculate new score
-        6. Send a response back to the client via the sendClient socket
-        7. Repeat steps 3-6 until the client closes the connection
-         */
-
-
-        // Server already started in the constructor
-
-        // Accept incoming connections
         log.info("Starting listening");
-        try (var socket = listenServer.accept()) {
+        try (Socket socket = listenServer.accept(); BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
-            // Repeat steps 3-6 until the client closes the connection
-            log.info("Starting loop");
-            while (!socket.isClosed()) {
-
-                // Read the input from the client
-                log.info("Initializing input stream");
-                try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    String inputLine;
-                    StringBuilder data = new StringBuilder();
-                    log.info("Waiting for input");
-                    while ((inputLine = in.readLine()) != null) {
-                        data.append(inputLine);
-                    }
-                    log.info("Input: " + data.toString());
-
-                    // Parse the JSON data
-                    JSONObject json = new JSONObject(data.toString());
-                    log.info("Received JSON: " + json);
-
-                    log.info("Setting new parameters");
-                    // Calculate new score
-                    this.sf.setWholeSchema(json.getString("wholeSchema"));
-                    this.sf.setPropCoeffPolicy(json.getString("propCoeffPolicy"));
-                    this.sf.setFixpoint(json.getString("fixpoint"));
-                    this.sf.setFDV1(json.getString("FDV1"));
-                    this.sf.setFDV2(json.getString("FDV2"));
-                    this.sf.setINDV1(json.getString("INDV1"));
-                    this.sf.setINDV2(json.getString("INDV2"));
-                    this.sf.setUCCV1(json.getString("UCCV1"));
-                    this.sf.setUCCV2(json.getString("UCCV2"));
-
-                    log.info("Calculating new score");
-                    var score = getAvgPerformance();
-                    log.info("New score: " + score);
-
-                    log.info("Building JSONObject");
-                    var response = new JSONObject();
-                    response.put("result", score);
-
-                    // Send a response back to the client using the sendClient socket
-                    log.info("Initializing output stream");
-                    try (OutputStream output = sendClient.getOutputStream(); PrintWriter writer = new PrintWriter(new OutputStreamWriter(output), true)) {
-                        log.info("Writing initial data");
-                        writer.println(response);
-                        log.info("Flushing writer");
-                        writer.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
+            log.info("Accepted connection, entering message loop");
+            while (true) {
+                log.info("Waiting for input");
+                String inputLine = in.readLine();
+                if (inputLine == null) {
+                    log.info("Connection closed by client");
+                    break;
                 }
+                log.info("Input: " + inputLine);
+
+                JSONObject json = new JSONObject(inputLine);
+                log.info("Received JSON: " + json);
+
+                // Update parameters based on received JSON
+                this.sf.setFixpoint(json.getString("fixpoint"));
+                this.sf.setFDV1(json.getString("FDV1"));
+                this.sf.setFDV2(json.getString("FDV2"));
+                this.sf.setINDV1(json.getString("INDV1"));
+                this.sf.setINDV2(json.getString("INDV2"));
+                this.sf.setUCCV1(json.getString("UCCV1"));
+                this.sf.setUCCV2(json.getString("UCCV2"));
+
+                log.info("Calculating new score");
+                double score = getAvgPerformance();
+                log.info("New score: " + score);
+
+                JSONObject response = new JSONObject();
+                response.put("score", score);
+
+                // Use the persistent sendWriter to send the response.
+                log.info("Sending response: " + response);
+                sendWriter.println(response);
+                sendWriter.flush();
             }
             log.info("Socket closed");
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error in listening: ", e);
         }
     }
+
 
     private double getAvgPerformance() {
         List<Double> performances = new ArrayList<>();
